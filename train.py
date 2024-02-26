fe1d8d3eb4f0d148e406b109f4fdf21afdc43918
Akshan Krithick <97239696+akshan-main@users.noreply.github.com>
2024-02-27 02:09:26 +0530

    Update train.py

--- train.py
+++ train.py
@@ -4,11 +4,8 @@ os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 from ultralytics import YOLO
 
 # loading a pre-trained model
-# if the first time loading a model, it will first download the model in the directory
-# available pre-trained models are YOLOv8n, YOLOv8s, YOLOv8m, YOLOv8l, YOLOv8x
 model = YOLO("yolov8n.pt")
 
-# will throw an exception if false
 model._check_is_pytorch_model()
 
 data_yaml_path = "data.yaml"

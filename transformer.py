ba67b4e8d38a8b2e293a7468bba04214560d4de4
Akshan Krithick <97239696+akshan-main@users.noreply.github.com>
2024-08-08 00:14:54 +0530

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -213,7 +213,6 @@ class MSDeformAttn(nn.Module):
         if d_model % n_heads != 0:
             raise ValueError(f'd_model must be divisible by n_heads, but got {d_model} and {n_heads}')
         _d_per_head = d_model // n_heads
-        # you'd better set _d_per_head to a power of 2 which is more efficient in our CUDA implementation
         assert _d_per_head * n_heads == d_model, '`d_model` must be divisible by `n_heads`'
 
         self.im2col_step = 64
